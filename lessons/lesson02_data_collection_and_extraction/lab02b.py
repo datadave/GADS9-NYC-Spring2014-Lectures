0ed3cc1511e1d6f1fa7b872ecb02b2791c85fe2f
podopie <epodojil@gmail.com>
2014-04-07 17:08:26 -0400

    update lecture and labs

--- lessons/lesson02_data_collection_and_extraction/lab02b.py
+++ lessons/lesson02_data_collection_and_extraction/lab02b.py
@@ -3,21 +3,23 @@ import urllib2
 
 class NYTimesScraper():
     def __init__(self, apikey):
+        # Creates a new NYTimesScraper Object using the apikey that was included.
         self.key = apikey
         self.url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'
-
-    def search(self, params={}):
+    def _build_params(self, params):
         if not params:
-            raise Exception('no search term!')
+            raise Exception('no search parameters!')
         else:
-            build_params = '&'.join([k + '=' + v for k,v in params.iteritems()])
-            build_params = build_params + '&api-key=%s' % self.key
-            built_url = self.url + build_params
-        req = urllib2.Request(built_url)
-        return json.loads(urllib2.urlopen(req).read())
+            return '&'.join([k + '=' + v for k,v in params.iteritems()])
+    def search(self, params={}):
+        url = self.url + self._build_params(params)
+        url = url + '&api-key=%s' % self.key
+        req = urllib2.Request(url)
+        data = urllib2.urlopen(req).read()
+        return json.loads(data)
 
 nytimes = NYTimesScraper(apikey='')
-articles = nytimes.search({'q':'coffee', 'begin_date': '20140101'})
+articles = api.search({'q':'malaysia', 'begin_date': '20140101'})
 
 for article in articles['response']['docs']:
     print '"' + '","'.join([article['byline']['person'][0]['lastname'],
@@ -26,3 +28,11 @@ for article in articles['response']['docs']:
         article['subsection_name'],
         article['word_count'],
         article['web_url']]) + '"'
+
+filename = 'nytimesdata.csv'
+writer = open(filename, 'w')
+
+# Fill in the column names here as one string
+writer.write('')
+for article in articles['response']['docs']:
+    # Write the article data you want to collect here
